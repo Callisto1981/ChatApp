e85c090c4414800693350c0f7ded9baacd424f79
Wayne <wabbruscatoorr@gmail.com>
2021-04-08 19:24:55 -0400

    Updating TheirMessage file

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -9,6 +9,21 @@ const TheirMessage = ({lastMessage, message}) => {
                     style={{backgroundImage: `url(${message?.sender?.avatar})`}}
                 />
             )}
+            
+            {message?.attachments?.length > 0 
+                ? (
+                    <img
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ margineLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                ) : (
+                    <div className="message" style={{ float: 'right', backgroundColor: '#CABCDC', margineLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                        {message.text}
+                    </div>
+                )
+            }
         </div>
     );
 }
