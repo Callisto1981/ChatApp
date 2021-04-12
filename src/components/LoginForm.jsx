90b11c041e56f34e67665b1a12f97a809b01dd60
Wayne <wabbruscatoorr@gmail.com>
2021-04-12 13:05:25 -0400

    Added div, form and inputs

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,9 +7,24 @@ const LoginForm = () => {
 
     const handleSubmit = () => {
 
+        
     }
 
     return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required/>
+                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 
 
