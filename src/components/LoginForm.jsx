2f2de25aafc8d2934f7a570ed85866dc8707f678
Wayne <wabbruscatoorr@gmail.com>
2021-04-13 15:32:02 -0400

    Added errer, seterror useState

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -18,7 +19,7 @@ const LoginForm = () => {
 
             window.location.reload()
         } catch (error) {
-
+            setError('Incorrect credentials!')
         }
 
     }
@@ -34,6 +35,7 @@ const LoginForm = () => {
                         <button type="submit" className="button">
                             <span>Start Chatting</span>
                         </button>
+                        <h2 className="error">{error}</h2>
                     </div>
                 </form>
             </div>
